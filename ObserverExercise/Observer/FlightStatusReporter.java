f5e639c13cb5a9d39b5fe8fdccd04b8c1321c166
Brigham Andersen <brighamband@gmail.com>
2022-01-10 15:18:59 -0700

    Second reporter/observer working

--- ObserverExercise/Observer/FlightStatusReporter.java
+++ ObserverExercise/Observer/FlightStatusReporter.java
@@ -1,6 +1,6 @@
 public class FlightStatusReporter implements FlightObserver {
     @Override
     public void update(Flight flight) {
-        System.out.println(flight.toString());
+        System.out.println("Status -- " + flight.toString());
     }
 }
