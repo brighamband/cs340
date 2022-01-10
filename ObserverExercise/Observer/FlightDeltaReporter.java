bf1e8b5d4a0b2cb28c9a24d66542ca75d0e609e6
Brigham Andersen <brighamband@gmail.com>
2022-01-10 15:18:39 -0700

    Second reporter/observer working

--- ObserverExercise/Observer/FlightDeltaReporter.java
+++ ObserverExercise/Observer/FlightDeltaReporter.java
@@ -1,7 +1,18 @@
 public class FlightDeltaReporter implements FlightObserver {
+
+    Flight lastFlight = new Flight();
+
     @Override
-    public void update(Flight flight) {
-        System.out.println("FLIGHT DELTA REPORTER");
-        System.out.println(flight.toString());
+    public void update(Flight newFlight) {
+        float deltaLongitude = newFlight.longitude - lastFlight.longitude;
+        float deltaLatitude = newFlight.latitude - lastFlight.latitude;
+        float deltaVelocity = newFlight.velocity - lastFlight.velocity;
+        float deltaAltitude = newFlight.geo_altitude - lastFlight.geo_altitude;
+
+        System.out.format("Deltas -- lon: %s, lat: %s, vel: %s, alt: %f\n",
+                deltaLongitude, deltaLatitude, deltaVelocity, deltaAltitude);
+
+        // Update last flight
+        lastFlight = newFlight;
     }
 }
--- ObserverExercise/Observer/FlightMonitor.java
+++ ObserverExercise/Observer/FlightMonitor.java
@@ -3,11 +3,11 @@ public class FlightMonitor {
 	
 	public static void main(String[] args) {
 		FlightStatusReporter fsr = new FlightStatusReporter();
-//		FlightDeltaReporter fdr = new FlightDeltaReporter();
+		FlightDeltaReporter fdr = new FlightDeltaReporter();
 
 		FlightFeed feed = new FlightFeed();
 		feed.add(fsr);
-//		feed.add(fdr);
+		feed.add(fdr);
 		feed.start();
 	}
 	
