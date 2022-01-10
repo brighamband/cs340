50788312bd0070d51cdc09668a528377213db96d
Brigham Andersen <brighamband@gmail.com>
2022-01-10 16:07:12 -0700

    Updated dir structure

---
+++ ObserverExercise/FlightMonitor.java
@@ -0,0 +1,14 @@
+
+public class FlightMonitor {
+	
+	public static void main(String[] args) {
+		FlightStatusReporter fsr = new FlightStatusReporter();
+		FlightDeltaReporter fdr = new FlightDeltaReporter();
+
+		FlightFeed feed = new FlightFeed();
+		feed.add(fsr);
+		feed.add(fdr);
+		feed.start();
+	}
+	
+}
