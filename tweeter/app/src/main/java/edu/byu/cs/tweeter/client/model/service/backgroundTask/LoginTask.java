f438505c273be16ae1c608b4ee24b5df19ad404b
Brigham Andersen <brighamband@gmail.com>
2022-03-09 20:43:40 -0700

    Hooked up login

--- tweeter/app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/LoginTask.java
+++ tweeter/app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/LoginTask.java
@@ -3,35 +3,47 @@ package edu.byu.cs.tweeter.client.model.service.backgroundTask;
 import android.os.Bundle;
 import android.os.Handler;
 
+import java.io.IOException;
+
+import edu.byu.cs.tweeter.client.model.net.ServerFacade;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.net.TweeterRemoteException;
+import edu.byu.cs.tweeter.model.net.request.LoginRequest;
+import edu.byu.cs.tweeter.model.net.response.LoginResponse;
 
 /**
  * Background task that logs in a user (i.e., starts a session).
  */
 public class LoginTask extends AuthenticateTask {
-//    private static final String LOG_TAG = "LoginTask";
+
+    static final String URL_PATH = "/login";
+
+    private ServerFacade serverFacade;
 
     public LoginTask(String username, String password, Handler messageHandler) {
         super(username, password, messageHandler);
     }
 
     @Override
-    protected void runTask() {
-        //  TODO Milestone 3
-    }
+    protected void runTask() throws IOException, TweeterRemoteException {
+        LoginRequest request = new LoginRequest(getUsername(), getPassword());
+        LoginResponse response = getServerFacade().login(request, URL_PATH);
 
-    private User getLoggedInUser() {
-        return getFakeData().getFirstUser();
-    }
-
-    private AuthToken getLoggedInUserAuthToken() {
-        return getFakeData().getAuthToken();
+        setUser(response.getUser());
+        setAuthToken(response.getAuthToken());
     }
 
     @Override
     protected void loadMessageBundle(Bundle msgBundle) {
-        msgBundle.putSerializable(USER_KEY, getLoggedInUser());
-        msgBundle.putSerializable(AUTH_TOKEN_KEY, getLoggedInUserAuthToken());
+        msgBundle.putSerializable(USER_KEY, getUser());
+        msgBundle.putSerializable(AUTH_TOKEN_KEY, getAuthToken());
+    }
+
+    public ServerFacade getServerFacade() {
+        if (serverFacade == null) {
+            serverFacade = new ServerFacade();
+        }
+        return new ServerFacade();
     }
 }
